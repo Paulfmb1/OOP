4946470815ff222306ffd9d293c4faf6878ec984
Paulfmb1 <90325126+Paulfmb1@users.noreply.github.com>
2022-06-15 16:44:05 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,48 +1,180 @@
 #include <iostream>
-#include "class.h"
 #include <fstream>
 
 using namespace std;
 
+struct Student {
+
+    int age = 0;
+    float progress = 0;
+    int course = 0;
+    char name[20]{};
+    char surname[20]{};
+    char patronymic[20]{};
+    bool gender = false;
+
+    Student() {}
+
+};
+
+
+
+void Add(fstream& file)
+{
+    Student student;
+
+    cout << "Name: ";
+    cin >> student.name;
+    file << student.name << " ";
+
+    cout << "Surname: ";
+    cin >> student.surname;
+    file << student.surname << " ";
+
+    cout << "Patronymic: ";
+    cin >> student.patronymic;
+    file << student.patronymic << " ";
+
+    cout << "Age: ";
+    cin >> student.age;
+    file << student.age << " ";
+  
+    cout << "Progress: ";
+    cin >> student.progress;
+    file << student.progress << " ";
+
+    cout << "Course: ";
+    cin >> student.course;
+    file << student.course << " ";
+
+    cout << "Gender: ";
+    cin >> student.gender;
+    if (student.gender)
+        file << "Man" << endl;
+    else
+        file << "Woman" << endl;
+
+    file.seekg(0, ios::end);
+    file.flush();
+
+}
+
+void Print(fstream& file)
+{
+    system("cls");
+    file.seekg(0, ios::beg);
+    cout << "----------ALL STUDENTS----------" << endl;
+    while (file)
+    {
+        char Human[6]{};
+        Student student;
+        file >> student.name;
+        file >> student.surname;
+        file >> student.patronymic;
+        file >> student.age;
+        file >> student.progress;
+        file >> student.course;
+        file >> Human;
+
+        if (!file.good()) 
+        {
+            break;
+        }
+        cout << student.name << " ";
+        cout << student.surname << " ";
+        cout << student.patronymic << " ";
+        cout << student.age << " ";
+        cout << student.progress << " ";
+        cout << student.course << " ";
+        cout << Human << endl;
+
+    }
+
+    cout << "-----------------------------" << endl;
+
+    file.clear();
+    file.seekg(0, ios::beg);
+
+}
+
+void Task(fstream& file, ofstream& result)
+{
+    file.seekg(0, ios::beg);
+    while (!file.eof()) {
+        char Human[6]{};
+        Student student;
+        file >> student.name;
+        file >> student.surname;
+        file >> student.patronymic;
+        file >> student.age;
+        file >> student.progress;
+        file >> student.course;
+        file >> Human;
+
+        if (!file.good()) {
+            break;
+        }
+
+        if (student.progress > 8.4)
+            result << student.name << " " << student.surname << " " << student.patronymic << endl;
+
+    }
+
+    file.clear();
+    file.seekg(0, ios::beg);
+    result.flush();
+}
+
+
+
 int main()
 {
-    ofstream out;
-    out.open("result.txt");
+    bool isWorking = true;
+    fstream OutputFile;
+    ofstream Result("Result.txt", ios::app);
+    OutputFile.open("1_4.txt", ios::in | ios::out | ios::app);
 
-    if (out.is_open())
+    while (isWorking)
     {
-        for (int i = 0; i < 20000; i++)
-        {
-            out << rand() % 10000 + 1 << endl;
-        }
-    }
+        cout << "1. Add\n"
+            << "2. Print\n"
+            << "3. Task\n"
+            << "4. Exit\n";
 
-    ifstream in("result.txt");
+        int choice;
+        cin >> choice;
 
-    int k = 0;
-    Vector<int> _vector(10001);
-    _vector.set_size(10001);
-    if (in.is_open())
-    {
-        while (!in.eof())
+        switch (choice)
         {
-            in >> k;
-            _vector[k]++;
-        }
-    }
-    in.close();
-
-    Vector<int> _vector2(10001);
-    _vector2.set_size(10001);
-    for (int i = 0, j = 0; i < 10001; i++)
-    {
-        if (_vector[i] > 1)
+        case 1:
         {
-            _vector2[j++] = i;
-            cout << i << "\n";
+            Add(OutputFile);
+            break;
+            system("cls");
         }
 
-    }
+        case 2:
+        {
+            Print(OutputFile);
+            break;
+        }
 
-    return 0;
+        case 3:
+        {
+            Task(OutputFile, Result);
+            break;
+        }
+
+
+        case 4:
+        {
+            OutputFile.close();
+            Result.close();
+            isWorking = false;
+            system("cls");
+            break;
+        }
+
+        }
+    }
 }
